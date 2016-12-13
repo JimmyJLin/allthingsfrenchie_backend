e234f4d82e222ff61033e05bc19da7576222a728
Jimmy Lin <usmcjimmy@gmail.com>
2016-12-12 22:42:07 -0500

    added

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('views', './views');
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 // routes
