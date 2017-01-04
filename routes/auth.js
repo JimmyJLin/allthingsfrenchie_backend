6cf161c15a43b4ce5e01f09a088880d0e47fdaf5
Jimmy Lin <usmcjimmy@gmail.com>
2017-01-03 21:33:15 -0500

    changed to status 400

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ auth.route('/login')
         message: 'user logged in'
       });
     } else {
-      res.send(200).json({ message: 'user is not currenlty signed in' });
+      res.send(400).json({ message: 'user is not currenlty signed in' });
     }
   })
   .post((req, res, next) => {
